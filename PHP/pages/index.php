ffa41c182fac28fd4d2894e7fa30334acb8de2ea
Pablo Guinard <93989499+PabloGuinard@users.noreply.github.com>
2021-11-15 14:59:45 +0100

    init

--- PHP/pages/index.php
+++ PHP/pages/index.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content=""width=device-width, initial-scale=1.8">
+    <title>Index</title>
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
+<header>
+
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
