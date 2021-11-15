a1e899bc95d84cd474f52dbf5bc280a0bb6f70ad
Louis Charnay <louis.charnay@orange.fr>
2021-11-15 17:35:16 +0100

    add image + scrollBar + informations to lecture page

--- PHP/lecture.php
+++ PHP/lecture.php
@@ -13,7 +13,49 @@
 
     </header>
     <main>
-
+        <div class="lecturePageVideo">
+            <img class="lecturePageImg" src="https://picsum.photos/1280/720" alt="image">
+            <span class="lecturePageProgressBar"></span>
+        </div>
+        <div class="lecturePageInformations">
+            <div class="lecturePageInformations_1">
+                <table>
+                    <tr>
+                        <td><b>Réalisateur :</b></td>
+                        <td>Mr Réalisateur</td>
+                    </tr>
+                    <tr>
+                        <td><b>Genre :</b></td>
+                        <td>Genre</td>
+                    </tr>
+                    <tr>
+                        <td><b>Date de sortie :</b></td>
+                        <td>19 octobre 2021</td>
+                    </tr>
+                    <tr>
+                        <td><b>Acteurs :</b></td>
+                        <td>Acteur 1</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Acteur 2</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Acteur 3</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="lecturePageInformations_2">
+                <h2>TITRE FILM</h2>
+                <h3>SOUS TITRE</h3>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi obcaecati alias itaque dolorum quibusdam excepturi modi nemo sequi nihil odit nisi consequatur numquam, esse assumenda sit aspernatur nam quis quia.</p>
+            </div>
+            <div class="lecturePageInformations_3">
+                <h3>Note Globale</h3>
+                <h3>Ma note</h3>
+            </div>
+        </div>
     </main>
     <footer>
 
