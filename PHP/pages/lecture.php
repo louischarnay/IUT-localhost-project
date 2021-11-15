189216d5399388b69bded9b3f49a0cff87229a89
Louis Charnay <louis.charnay@orange.fr>
2021-11-15 15:01:51 +0100

    init lecture.php

---
+++ PHP/pages/lecture.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>projet Methodo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
