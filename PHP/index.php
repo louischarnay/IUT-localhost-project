ba104f1e1d58a22c7bd8bad89d74153ee0bb648a
Pablo Guinard <93989499+PabloGuinard@users.noreply.github.com>
2021-11-15 17:45:44 +0100

    commit fin de séance 15/11

---
+++ PHP/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content=""width=device-width, initial-scale=1.8">
+    <title>Index</title>
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
+<header>
+    <div class="indexTitre">
+        <div class="indexLogo">
+            <ion-icon name="diamond-outline"></ion-icon>
+        </div>
+        <h1>Second Films</h1>
+    </div>
+    <div class="indexDivBoutons">
+        <a href="#" class="indexBoutonMenu">Profil</a>
+        <a href="#" class="indexBoutonMenu">Déconnexion</a>
+        <a href="#" class="indexBoutonMenu">Rechercher</a>
+        <div class="indexInput">
+            <input type="text" placeholder="Rechercher">
+        </div>
+    </div>
+</header>
+<main>
+    <div class="indexRecommandation">
+        <img src="https://picsum.photos/180/240" alt="affiche film recommandé" class="indexAfficheFilmRecommande">
+        <div class="indexDescriptionFilm">
+                <h2>FILM RECOMMANDE</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur at consequatur deserunt dignissimos
+                    dolore eius in modi molestiae nesciunt non obcaecati officia optio, placeat quas qui quia quo repellat
+                    temporibus unde vitae voluptas voluptatibus voluptatum! Cumque deserunt dicta error fuga fugiat illum modi
+                    mollitia. Aliquid dolorum iure maxime modi sapiente, tenetur vel voluptatibus voluptatum? Ab adipisci
+                    eveniet fugit magnam odio provident quod temporibus. Adipisci aspernatur commodi consectetur consequatur
+                    corporis cumque debitis dicta dignissimos, enim, et fuga ipsa ipsam iste maiores maxime minus molestiae
+                    neque nisi officiis omnis recusandae sint soluta veniam! Dignissimos doloribus est facere ipsa nam
+                    praesentium repudiandae.</p>
+        </div>
+    </div>
+    <div class="indexCategorie">
+        <h3>CATEGORIE 1</h3>
+        <div class="indexEtoiles"></div>
+        <div class="indexFlecheGauche"></div>
+        <img src="https://picsum.photos/120/160" alt="affiche film 1" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 2" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 3" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 4" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 5" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 6" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 7" class="indexAfficheFilm">
+        <div class="indexFlecheDroite"></div>
+    </div>
+    <div class="indexCategorie">
+        <h3>CATEGORIE 2</h3>
+        <div class="indexEtoiles"></div>
+        <div class="indexFlecheGauche"></div>
+        <img src="https://picsum.photos/120/160" alt="affiche film 1" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 2" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 3" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 4" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 5" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 6" class="indexAfficheFilm">
+        <img src="https://picsum.photos/120/160" alt="affiche film 7" class="indexAfficheFilm">
+        <div class="indexIlecheDroite"></div>
+    </div>
+</main>
+<footer>
+
+</footer>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+</body>
+</html>
