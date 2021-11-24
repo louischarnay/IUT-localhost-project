9288dea8a9f226b40b9cd00b14280ca043f0835b
Benjamin-Eldo <84413803+Benjamin-Eldo@users.noreply.github.com>
2021-11-24 16:12:00 +0100

    test intégration PHP

--- guillaume.py
+++ guillaume.py
@@ -1,9 +1,8 @@
 from collections import defaultdict
-
+from surprise import Reader
 from surprise import SVD
 from surprise import Dataset
-from surprise import Reader
-from surprise.model_selection import GridSearchCV
+
 
 def get_top_n(predictions, n=10):
     """Return the top-N recommendation for each user from a set of predictions.
@@ -34,6 +33,8 @@ def get_top_n(predictions, n=10):
 
 # First train an SVD algorithm on the movielens dataset.
 
+
+
 file_path = 'ratings.csv'
 
 reader = Reader(line_format='user item rating timestamp', sep=',')
@@ -41,23 +42,14 @@ reader = Reader(line_format='user item rating timestamp', sep=',')
 print("loading file")
 data = Dataset.load_from_file(file_path, reader=reader)
 
+
 trainset = data.build_full_trainset()
-
-algo=SVD()
-
-
-param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005],
-              'reg_all': [0.4, 0.6]}
-
-algo = GridSearchCV(SVD, param_grid, measures=['rmse', 'mae'], cv=3)
-algo.fit(data)
-
-algo = algo.best_estimator['rmse']
-algo.fit(data.build_full_trainset())
+algo = SVD()
+algo.fit(trainset)
 
 # Than predict ratings for all pairs (u, i) that are NOT in the training set.
-print("training...")
-predictions = algo.test(data)
+testset = trainset.build_anti_testset()
+predictions = algo.test(testset)
 
 top_n = get_top_n(predictions, n=10)
 
