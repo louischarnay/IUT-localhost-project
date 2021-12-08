b46579d4ec27d238361230cc1a0b6fde6992e815
Pablo Guinard <93989499+PabloGuinard@users.noreply.github.com>
2021-12-08 14:45:51 +0100

    traitement incription

--- PHP/class/Db.php
+++ PHP/class/Db.php
@@ -5,7 +5,7 @@ class db
 public $pdo;
 function __construct(){
     try {
-        $this->pdo = new PDO('sqlite:' . __DIR__ . '/../database/base.db');
+        $this->pdo = new PDO('sqlite:' . __DIR__ . '/../database/base');
     }
     catch (PDOException $e) {
         echo 'Connexion échouée : ' . $e->getMessage();
@@ -13,17 +13,15 @@ function __construct(){
 }
 
  public function createAccount(string $email, string $password){
-    try{
     $sth = $this->pdo->prepare("SELECT * FROM Accounts WHERE email= :email");
-    if($sth->execute(["email" => $email])){
+     $sth->execute(["email" => $email]);
+    $result = $sth->fetch();
+    if($result){
         return 1;
-    }}catch (PDOException $e){
-        echo $e->getMessage();
     }
-    $sth = $this->pdo->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+    $sth = $this->pdo->prepare("INSERT INTO Accounts(email, password) VALUES(:email, :password)");
     if($sth->execute(["email" => $email, "password" => $password])){
         return 0;
     }
-    return 2;
  }
 }
--- PHP/inscriptionTraitement.php
+++ PHP/inscriptionTraitement.php
@@ -2,4 +2,18 @@
 session_start();
 include "class/Db.php";
 $db = new Db();
-$db->createAccount($_POST["username"], $_POST["password"]);
+if($_POST["password"] == $_POST["passwordConfirm"]) {
+    $result = $db->createAccount($_POST["username"], $_POST["password"]);
+    if($result == 0){
+        $_SESSION["testIncription"] = "good";
+        header("Location: ../index.php");
+    }
+    else if($result == 1){
+        $_SESSION["testIncription"] = "badEmail";
+        header("Location: ../inscription.php");
+    }
+}
+else{
+    $_SESSION["testIncription"] = "badPassword";
+    header("Location: ../inscription/php");
+}
