579e4706e7f3375e04e80d68022b898717841348
Pablo Guinard <93989499+PabloGuinard@users.noreply.github.com>
2021-12-08 11:14:50 +0100

    on recommence

--- PHP/class/Db.php
+++ PHP/class/Db.php
@@ -9,22 +9,22 @@ function __construct(){
  public function createAccount(string $email, string $password){
     try {
         $pdo = new PDO('sqlite: ' . __DIR__ . ' ../database/base.db');
+        $sth = $pdo->prepare("SELECT * FROM Users WHERE username= :email");
+        $sth->execute([
+            "email" => $email
+        ]);
+        if($sth->fetch()){
+            $sth = $this->pdo->prepare("INSERT INTO Users (email, passord)");
+            $sth->execute([
+                "email"=>$email,
+                "password"=>$password
+            ]);
+            return 0;
+        }
     }
  catch (PDOException $e) {
          echo 'Connexion échouée : ' . $e->getMessage();
      }
-    $sth = $pdo->prepare("SELECT * FROM Users WHERE username= :email");
-    $sth->execute([
-        "email" => $email
-    ]);
-    if($sth->fetch()){
-        $sth = $this->pdo->prepare("INSERT INTO Users (email, passord)");
-        $sth->execute([
-            "email"=>$email,
-            "password"=>$password
-        ]);
-        return 0;
-    }
     return 1;
  }
 }
